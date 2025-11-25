1433c26c2b4a0e5d4ab964785fb14b78fd169d5a
tanmoychakraborty00001-a11y <tanmoy.chakraborty00001@gmail.com>
2025-11-25 13:49:52 +0000

    Added Ex02_01 code

---
+++ python-exercises/Chapter02_Variables_Input_Datatypes/Ex02_01.py
@@ -0,0 +1,30 @@
+# Ex02_01 — Basic Variables
+# 1) Sum of two variables
+a = 10
+b = 20
+print("Sum is: ", a+b)
+
+# 2) Greeting with name
+name = "Tanmoy"
+print("Hello! ", name)
+
+# 3) Total price of 3 items
+price1 = 100
+price2 = 250
+price3 = 340
+total = price1 + price2 + price3
+print("Total price: ", total)
+
+# 4) Swaping using third variable
+a = 5
+b = 10
+temp = a
+a = b
+b = temp
+print(a, b)
+
+# 5) Swapping without third variable
+a = 5
+b = 10
+a, b = b, a
+print(a, b)
