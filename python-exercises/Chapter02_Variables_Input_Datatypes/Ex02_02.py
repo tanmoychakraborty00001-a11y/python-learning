db236e9d8508fe002b18a984526e992d85e8c3e0
tanmoychakraborty00001-a11y <tanmoy.chakraborty00001@gmail.com>
2025-11-25 17:17:54 +0000

    Added Ex02_02 exercises

---
+++ python-exercises/Chapter02_Variables_Input_Datatypes/Ex02_02.py
@@ -0,0 +1,21 @@
+# Ex02_02 - Typecasting and Input
+
+# 1) Converting input string to int
+num = int(input("Enter any number: "))
+print("Square of the number: ", num * num)
+
+# 2) Adding two numbers taking user inputs
+a = int(input("Enter first number: "))
+b = int(input("Enter seconf number: "))
+print("Sum of the numbers: ", a + b)
+
+# 3) Years to reach 100
+age = int(input("Enter your age: "))
+years = 100 - age
+print("You will be 100 in ", years, "years")
+
+# 4) Area of rectangle
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+area = length * breadth
+print("Area of the rectangle: ", area)
